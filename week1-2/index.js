db3fed7da1717f7b2125c94859aa3025dabf90ad
Andrew Liu <drewfle@Tetis-Air.nyc.rr.com>
2019-09-16 23:21:06 -0400

    completed wk1-2

--- week1-2/index.js
+++ week1-2/index.js
@@ -1,56 +1,67 @@
 /**
+ * Student: Andrew Liu
  * Wall Drawing #130 (1972) Grid and arcs from four corners.
+ *
+ * Just realized we have two assignments instead of one. So quickly designed a recursive
+ * drawGrid function and a simple drawArcs function.
  */
 
 const eighteenPercentGray = 124;
 let centerX;
 let centerY;
-let slope;
 
 function setup() {
   centerX = windowWidth / 2;
   centerY = windowHeight / 2;
-  slope = windowHeight / windowWidth;
+  noFill();
   createCanvas(windowWidth, windowHeight);
-  noLoop();
 }
 
 function draw() {
-  background(eighteenPercentGray);
-  keepDrawingLines();
+  background(255);
+  stroke(eighteenPercentGray);
+  drawGrid();
+  stroke(0);
+  drawArcs();
 }
-function keepDrawingLines(line1 = {}, line2 = {}, i = 1) {
+
+function drawArcs() {
+  arc(centerX, 0, windowWidth, windowHeight, 0, PI);
+  arc(windowWidth, centerY, windowWidth, windowHeight, HALF_PI, HALF_PI);
+  arc(centerX, windowHeight, windowWidth, windowHeight, PI, 0);
+  arc(0, centerY, windowWidth, windowHeight, HALF_PI, HALF_PI);
+}
+
+function drawGrid() {
+  const deltaY = centerY * Math.tan(radians(45));
+  const centerX = windowWidth / 2;
+  const x1 = centerX - deltaY;
+  const y1 = windowHeight;
+  const x2 = centerX + deltaY;
+  const y2 = 0;
+  const x3 = centerX - deltaY;
+  const y3 = 0;
+  const x4 = centerX + deltaY;
+  const y4 = windowHeight;
+  line(centerX - deltaY, windowHeight, centerX + deltaY, 0);
+  line(centerX - deltaY, 0, centerX + deltaY, windowHeight);
+  keepDrawingLines(x1, y1, x2, y2, x3, y3, x4, y4, 1);
+}
+
+function keepDrawingLines(x1, y1, x2, y2, x3, y3, x4, y4, i = 1) {
   const interval = 30 * i;
   const deltaX = interval / Math.cos(radians(45));
-  if (i > 50) {
-    return;
-  }
   if (
-    line1.x2 - deltaX < 0 ||
-    line1.x1 + deltaX > windowWidth ||
-    line2.x1 + deltaX > windowWidth ||
-    line2.x2 - deltaX < 0
+    x2 - deltaX < 0 ||
+    x1 + deltaX > windowWidth ||
+    x3 + deltaX > windowWidth ||
+    x4 - deltaX < 0
   ) {
     return;
   }
-  if (!Object.keys(line1).length && !Object.keys(line2).length) {
-    const deltaY = centerY * Math.tan(radians(45));
-    line1.x1 = centerX - deltaY;
-    line1.y1 = windowHeight;
-    line1.x2 = centerX + deltaY;
-    line1.y2 = 0;
-    line(...Object.values(line1));
-    line2.x1 = centerX - deltaY;
-    line2.y1 = 0;
-    line2.x2 = centerX + deltaY;
-    line2.y2 = windowHeight;
-    line(...Object.values(line2));
-  }
-
-  line(line1.x1 - deltaX, windowHeight, line1.x2 - deltaX, 0);
-  line(line1.x1 + deltaX, windowHeight, line1.x2 + deltaX, 0);
-  line(line2.x1 + deltaX, 0, line2.x2 + deltaX, windowHeight);
-  line(line2.x1 - deltaX, 0, line2.x2 - deltaX, windowHeight);
-
-  keepDrawingLines(line1, line2, ++i);
+  line(x1 - deltaX, windowHeight, x2 - deltaX, 0);
+  line(x1 + deltaX, windowHeight, x2 + deltaX, 0);
+  line(x3 + deltaX, 0, x4 + deltaX, windowHeight);
+  line(x3 - deltaX, 0, x4 - deltaX, windowHeight);
+  keepDrawingLines(x1, y1, x2, y2, x3, y3, x4, y4, ++i);
 }
