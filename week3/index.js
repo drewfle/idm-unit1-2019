37b7500f8a09b9bfbdd6f772141a81575d68741d
Andrew Liu <andrewliustudio@gmail.com>
2019-09-17 23:45:21 -0400

    complete week3

--- week3/index.js
+++ week3/index.js
@@ -1,32 +1,59 @@
+/**
+ * Student: Andrew Liu
+ * Making linear micro moire patterns.
+ */
+
+const eighteenPercentGray = 124;
 let halfW;
 let slope;
+let interval;
+let movingY;
+let increment = 1;
+let oneThirdH;
 
 function setup() {
   halfW = windowWidth / 2;
   slope = halfW / windowHeight;
+  interval = windowHeight / 123;
+  movingY = 0;
   createCanvas(windowWidth, windowHeight);
-  background(124);
-  console.log('foo');
+  background(255);
 }
 function draw() {
+  background(255);
   drawTriangle();
+  moveLine();
 }
 
 function drawTriangle() {
-  const interval = 5;
   const loX = halfW;
   const hiY = 0;
   beginShape(LINES);
-  for (let loY = interval; loY <= windowHeight ; loY += interval) {
-   // const delta = 
+  for (let loY = interval; loY <= windowHeight; loY += interval) {
+    stroke(255 - (loY * 255) / windowHeight);
     const hiRightX = loX + slope * loY;
     const hiLeftX = loX - slope * loY;
-    //vertex(loX, loY);
     vertex(hiRightX, hiY);
-    //vertex(loX, loY);
-
+    vertex(loX, loY);
     vertex(hiLeftX, hiY);
     vertex(loX, loY);
   }
+  endShape();
+}
+
+function moveLine() {
+  stroke(eighteenPercentGray);
+  if (movingY === windowHeight) {
+    increment = -1;
+  }
+  if (movingY === 0) {
+    increment = 1;
+  }
+  movingY += increment;
+  beginShape(LINES);
+  vertex(windowWidth, 0);
+  vertex(halfW, movingY);
+  vertex(0, 0);
+  vertex(halfW, movingY);
   endShape();
 }
