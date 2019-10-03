674e9df278e9a396e36db9983b1a595a18f01926
Andrew Liu <andrewliustudio@gmail.com>
2019-10-02 22:20:36 -0400

    added wk4

--- week4/index.js
+++ week4/index.js
@@ -1,42 +1,6 @@
-/*
-From Joseph Albers' Interaction of color:
-
-3 colors appear as 2
-[...] the center squares look like the grounds exchanged. But they are of the same color,
-precisely alike, and at the same time refer to the neighboring grounds. 
-The true color of the two central squares therefore becomes unrecognizable, as it loses its identity.
-
-What color relatedness makes 3 colors look like 2?
-
-Your assignment:
-Study this visual example. 
-Write your observations in a notepad while looking at the colors. 
-
-- What is the perceived "brightness" relationship between the two rectangular backgrounds?
-The lefthand background has less perceived brightness then its righthand counterpart.
-
-- Are the small squares brighter or darker than their respective grounds?
-As deceiptive as human perception could be, the lefthand small square is much brighter than both of its background and its small square counterpart. And the righthand small square is much darker than both of its background and the righthand counterpart.
- 
-- Are they more or less "saturated" than their respective grounds?
-Perhaps saturation is also a relative attribute in color perception. Both of the small squares have same amount of saturation comparing with their respective backgrounds.
-
-Now, fork this example and copy your observations into a comment at the top of your code like I'm
-doing here.
-
-Replace the color1, color2, and color3 values on lines 42-44 with your own variation.
-You must use HSB numbers as color values, and you must use three colors that "appear" as 2 perceptual colors.
-
-
-Tips: 
-Use built-in variables, mouseX, and mouseY as temporary replacements for your hard
-coded numbers, and console.log(mouseX, mouseY); to record those numbers for later replacement
-
-Remember that the color argument with the most impact on its position in the color wheel is the first one (hue).
-Are the colors in this example analogous or complementary? 
-If you answered "analogous", try re-creating the effect with complementary colors/
-If you answered "complementary", tr re-creating the effect with analogous colors.
-*/
+/**
+ * Student: Andrew Liu
+ */
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -46,64 +10,66 @@ function setup() {
 }
 
 function draw() {
-  const {
-    drawLeftBackground,
-    drawRightBackground,
-    drawLeftSmallSquare,
-    drawRightSmallSquare
-  } = squares;
+  // Found the HSB value of international Klein blue here: https://www.colorhexa.com/002fa7
+  const kleinBlueHue = 223.1;
+  const kleinBlue = color(kleinBlueHue, 100, 65.5);
 
-  drawLeftBackground(colors.leftBkdg);
-  drawRightBackground(colors.rightBkdgAndSq);
-  drawLeftSmallSquare(colors.leftSq);
-  drawRightSmallSquare(colors.rightBkdgAndSq);
+  // Setup colors
+  const smallSq = color(kleinBlueHue, 100, 60);
+  const leftBkdg = color(kleinBlueHue, 80, 35);
+  const rightBkdg = kleinBlue;
+
+  // Define shapes
+  const upperLeft = new JosephShapes(smallSq, leftBkdg);
+  const upperRight = new JosephShapes(smallSq, rightBkdg, width / 2);
+  const lowerLeft = new JosephShapes(smallSq, rightBkdg, 0, height / 2);
+  const lowerRight = new JosephShapes(smallSq, leftBkdg, width / 2, height / 2);
+
+  // Draw shapes
+  upperLeft.draw();
+  upperRight.draw();
+  lowerLeft.draw();
+  lowerRight.draw();
 }
-
-class AlbersShapes {
-  constructor() {
-    this.backgroundColor
-  }
-  draw() {
-  }
-  drawBackground() {
-    fill(colorValue);
-    rect(0, 0, width / 2, height);
-  }
-  drawSmallSquare() {
-    fill(colorValue);
-    rect(width / 4, height / 4, width / 16, width / 16);
-  }
-}
-
-const colors = {
-  leftBkdg: color(121, 72, 55),
-  leftSq: color(120, 90, 80),
-  rightBkdgAndSq: color(120, 91, 68)
-};
 
 /**
- * Instead of using comments, I am trying to use explicit
- * function names to denote the "action and behavior" of the
- * code in the draw function.
- *
- * color global
+ * Designed a class to draw Joseph's color shape example
+ * at different locations on screen with different sizes.
+ * Prefixing '_' on class variables and methods to denote
+ * them as private properties. Other options include using
+ * Symbol and even more contrived workaround.
  */
-const squares = {
-  drawLeftBackground(colorValue) {
-    fill(colorValue);
-    rect(0, 0, width / 2, height);
-  },
-  drawRightBackground(colorValue) {
-    fill(colorValue);
-    rect(width / 4, height / 4, width / 16, width / 16);
-  },
-  drawLeftSmallSquare(colorValue) {
-    fill(colorValue);
-    rect(width / 2, 0, width / 2, height);
-  },
-  drawRightSmallSquare(colorValue) {
-    fill(colorValue);
-    rect(width * 0.75, height / 4, width / 16, width / 16);
+class JosephShapes {
+  constructor(
+    smallSquareColor,
+    backgroundColor,
+    originX = 0,
+    originY = 0,
+    shapeWidth = width / 2,
+    shapeHeight = height / 2
+  ) {
+    this._smallSquareColor = smallSquareColor;
+    this._backgroundColor = backgroundColor;
+    this._originX = originX;
+    this._originY = originY;
+    this._shapeWidth = shapeWidth;
+    this._shapeHeight = shapeHeight;
   }
-};
-
+  draw() {
+    this._drawBackground();
+    this._drawSmallSquare();
+  }
+  _drawSmallSquare() {
+    fill(this._smallSquareColor);
+    rect(
+      this._originX + this._shapeWidth / 2,
+      this._originY + this._shapeHeight / 4,
+      this._shapeWidth / 8,
+      this._shapeWidth / 8
+    );
+  }
+  _drawBackground() {
+    fill(this._backgroundColor);
+    rect(this._originX, this._originY, this._shapeWidth, this._shapeHeight);
+  }
+}
