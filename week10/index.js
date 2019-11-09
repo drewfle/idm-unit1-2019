df58d30136de2fc645619ca80ff482f994be5b57
Andrew Liu <andrewliustudio@gmail.com>
2019-11-09 13:25:05 -0500

    completed wk10

--- week10/index.js
+++ week10/index.js
@@ -1,25 +1,94 @@
 /**
- * replace all nouns and adjectives in the text with random nouns and
- * adjectives. If there is rhyme scheme in the poem, try changing it to
- * match your new words.
- *
- * open -n -a /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --user-data-dir="/tmp/chrome_dev_test" --disable-web-security
- *
- * 7.	JJ	Adjective
- * 8.	JJR	Adjective, comparative
- * 9.	JJS	Adjective, superlative
- * 12.	NN	Noun, singular or mass
- * 13.	NNS	Noun, plural
+ * Renders reworded clauses one by one in the P5.js draw loop. Please scroll the
+ * screen when the reworded text is approaching the bottom of browser.
  */
 
 /**
  * Global variables
  */
-const allPoses = ["jj", "jjr", "jjs", "nn", "nns"];
-const lexicon = new RiLexicon();
-const ravenLinesLive = [];
+const adjectivePoses = ["jj", "jjr", "jjs"];
+const nounPoses = ["nn", "nns"];
+const openprocessingHeaderHeight = 700;
+let lexicon;
+let rewordRaven;
 let ravenClauses;
-let theRaven = "";
+
+/**
+ * Custom classes
+ */
+
+class Reword {
+  constructor(oldClauses, posesToReplace) {
+    this.oldClauses = oldClauses;
+    this.posesToReplace = posesToReplace;
+    this.rewordedClauses = [];
+    this.currentClauseIndex = 0;
+  }
+  /**
+   * Renders and reword clauses incrementally.
+   */
+  renderClausesIncrementally() {
+    // rewordClause() is an expensive operation, thus we only need to call it
+    // for one clause at a time.
+    if (this.currentClauseIndex < this.oldClauses.length) {
+      const rewordedClause = this.rewordClause(
+        this.oldClauses[this.currentClauseIndex++]
+      );
+      this.rewordedClauses.push(rewordedClause);
+    }
+    this.rewordedClauses.forEach((clause, i) => {
+      text(clause, 10, openprocessingHeaderHeight + i * 15);
+    });
+  }
+  /**
+   * Rewords a clause with correct formatting or changes it to a new line
+   * because new lines in the input text are converted to empty strings in
+   * loadStrings().
+   * @param {string} clause
+   */
+  rewordClause(clause) {
+    if (clause !== "") {
+      const clauseTokens = RiTa.tokenize(clause);
+      this.rewordClauseTokens(clauseTokens);
+      const rewordedClause = RiTa.untokenize(clauseTokens);
+      const isBeginClause = clause[0] === clause[0].toUpperCase();
+      return isBeginClause ? `\n${rewordedClause}` : rewordedClause;
+    } else {
+      return "\n";
+    }
+  }
+  /**
+   * Conditionally rewords clause tokens so that we can retain rhymes.
+   * @param {array} clauseTokens RiTa tokens
+   */
+  rewordClauseTokens(clauseTokens) {
+    clauseTokens.forEach((token, i) => {
+      const [tokenPos] = RiTa.getPosTags(token);
+      const posIndex = this.posesToReplace.indexOf(tokenPos);
+      // Don't do anything for parts of speech that are not in posesToReplace.
+      if (posIndex === -1) {
+        return;
+      }
+      const isClauseEnd =
+        i + 1 < clauseTokens.length
+          ? RiTa.isPunctuation(clauseTokens[i + 1])
+          : false;
+      // Key step to retain rhyme in a cause.
+      if (isClauseEnd) {
+        const rhymeWords = lexicon.rhymes(token);
+        const samePosRhymeWord = rhymeWords.find(
+          rhymeWord =>
+            rhymeWord !== token && RiTa.getPosTags(rhymeWord) === tokenPos
+        );
+        const randomWord = lexicon.randomWord(tokenPos);
+        clauseTokens[i] = samePosRhymeWord ? samePosRhymeWord : randomWord;
+      } else {
+        const randomWord = lexicon.randomWord(tokenPos);
+        clauseTokens[i] = randomWord;
+      }
+    });
+  }
+}
 
 /**
  * P5.js hooks
@@ -31,54 +100,19 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  // Note on the canvas height, we need to allocated enough space to fit the
+  // entire text to be rendered
+  createCanvas(
+    windowWidth,
+    openprocessingHeaderHeight + (ravenClauses.length + 1) * 15
+  );
   fill(0);
   textSize(12);
-  ravenClauses = ravenClauses.slice(0, 10);
+  lexicon = new RiLexicon();
+  rewordRaven = new Reword(ravenClauses, [...adjectivePoses, ...nounPoses]);
 }
 
 function draw() {
-  if (!ravenClauses.length) {
-    noLoop();
-  }
   background(255);
-  const ravenClause = ravenClauses.shift();
-  if (ravenClause) {
-    const ravenTokens = RiTa.tokenize(ravenClause);
-    rewordClause(ravenTokens);
-    const newRavenClause = RiTa.untokenize(ravenTokens);
-    const isBeginClause = newRavenClause[0] === newRavenClause[0].toUpperCase();
-    theRaven += isBeginClause ? `\n${newRavenClause}` : newRavenClause;
-  } else {
-    theRaven += "\n";
-  }
-  text(theRaven, 10, 10, width - 20, height - 20);
-}
-
-function rewordClause(clause) {
-  const ravenTokens = clause;
-  ravenTokens.forEach((token, i) => {
-    const tokenPos = RiTa.getPosTags(token)[0];
-    const posIndex = allPoses.indexOf(tokenPos);
-    if (posIndex === -1) {
-      return;
-    }
-    const isClauseEnd =
-      i + 1 < ravenTokens.length
-        ? RiTa.isPunctuation(ravenTokens[i + 1])
-        : false;
-    const rhymeWord = RiTa.rhymes(token);
-    if (isClauseEnd) {
-      const rhymeWords = RiTa.rhymes(token);
-      const samePosRhymeWord = rhymeWords.find(
-        rhymeWord =>
-          rhymeWord !== token && RiTa.getPosTags(rhymeWord) === tokenPos
-      );
-      const randomWord = RiTa.randomWord(tokenPos);
-      ravenTokens[i] = samePosRhymeWord ? samePosRhymeWord : randomWord;
-    } else {
-      const randomWord = RiTa.randomWord(tokenPos);
-      ravenTokens[i] = randomWord;
-    }
-  });
+  rewordRaven.renderClausesIncrementally();
 }
