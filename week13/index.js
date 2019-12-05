07d03039c2ffd579fc722955e57cb82fc15f80c7
Andrew Liu <andrewliustudio@gmail.com>
2019-12-05 00:14:38 -0500

    wip

--- week13/index.js
+++ week13/index.js
@@ -6,51 +6,27 @@
  * cache by image name
  * url.match(/([^\/]+)(?=\.\w+$)/)[0]
  */
+
+// TODO: change back to cloud url
 const bodyPartsBaseUrl = "assets";
 // const bodyPartsBaseUrl =
 //   "https://openprocessing-usercontent.s3.amazonaws.com/files/user188449/visual803548/h7742c0df291332632e44a2cdbca0541d";
 
-// Creates unique id string like 5519497453702618, without this from
-// the second browser load and onwards the console will output:
-// Uncaught (in promise) DOMException: Failed to execute 'texImage2D' on 'WebGL2RenderingContext': Tainted canvases may not be loaded.
-const uuid = `${Math.random()}`.slice(2);
 const bodyParts = [
-  { url: `${bodyPartsBaseUrl}/arm.jpg?${uuid}`, id: "arm" },
-  { url: `${bodyPartsBaseUrl}/leg.jpg?${uuid}`, id: "leg" }
+  { url: `${bodyPartsBaseUrl}/arm.jpg` },
+  { url: `${bodyPartsBaseUrl}/leg.jpg` }
+  // { url: `${bodyPartsBaseUrl}/body.jpg` }
 ];
-// let bodyPartsHtmlImgs;
 let areExternalScriptsLoaded = false;
+let input;
 
 function setup() {
-  // createCanvas(windowWidth, windowHeight);
-  bodyParts.forEach(bodyPart => {
-    // attribute() must be called before id(), see:
-    // https://github.com/ml5js/ml5-library/issues/217
-    bodyPart.img = createImg(bodyPart.url)
-      .attribute("crossorigin", "anonymous")
-      .id(bodyPart.id)
-      .position(0, windowHeight / 2);
-    console.log(bodyPart.img);
-    // display: none; width: 0px; height: 0px; position: absolute; left: 0px; top: 0px;
-    bodyPart.canvas = createGraphics(bodyPart.img.width, bodyPart.img.height)
-      // .attribute("width", bodyPart.img.width)
-      // .attribute("height", bodyPart.img.height)
-      // .style("display", null)
-      // .style("width", null)
-      // .style("height", null)
-      // .style("position", null)
-      // .style("left", null)
-      // .style("top", null)
-      .id(`canvas-${bodyPart.id}`)
-      .position(0, 0);
-    const x = document.getElementById(`canvas-${bodyPart.id}`);
-    x.style.removeProperty("display");
-    x.style.removeProperty("width");
-    x.style.removeProperty("height");
-    x.style.removeProperty("position");
-    x.style.removeProperty("left");
-    x.style.removeProperty("top");
-  });
+  noCanvas();
+  initializeBodyParts();
+  // TODO: move the file input into a banner on the bottom
+  input = createFileInput(handleFile);
+  input.position(0, 0);
+  frameRate(1);
 }
 
 function draw() {
@@ -59,29 +35,70 @@ function draw() {
     return;
   }
   loadAndPredict();
-  noLoop();
+  // noLoop();
+}
+
+function handleFile(file) {
+  if (file.type !== "image") {
+    return;
+  }
+  const bodyPart = {};
+  createBodyPart(bodyPart, file.data);
+  bodyParts.push(bodyPart);
+}
+
+function initializeBodyParts() {
+  bodyParts.forEach(bodyPart => {
+    createBodyPart(bodyPart);
+  });
+}
+
+function createBodyPart(bodyPart, fileData = undefined) {
+  const uuid = `${Math.random()}`.slice(2);
+  // Creates unique id string like 5519497453702618, without this from
+  // the second browser load and onwards the console will output:
+  // Uncaught (in promise) DOMException: Failed to execute 'texImage2D' on 'WebGL2RenderingContext': Tainted canvases may not be loaded.
+  bodyPart.img = createImg(fileData ? fileData : `${bodyPart.url}?${uuid}`)
+    // attribute() must be called before id(), see:
+    // https://github.com/ml5js/ml5-library/issues/217
+    .attribute("crossorigin", "anonymous")
+    // We need an unique id for each image, otherwise p5.js will keep
+    // using the existing one
+    .id(uuid)
+    .hide();
+  // display: none; width: 0px; height: 0px; position: absolute; left: 0px; top: 0px;
+  // TODO: use remove() to gc
+  bodyPart.canvas = createGraphics(bodyPart.img.width, bodyPart.img.height)
+    .id(`canvas-${uuid}`)
+    .position(0, 0)
+    .show()
+    .style("width", "")
+    .style("height", "");
 }
 
 async function loadAndPredict() {
-  bodyParts.forEach(async ({ id }) => {
-    const img = document.getElementById(id);
+  bodyParts.forEach(async ({ id, img, canvas }) => {
     const net = await bodyPix.load();
-    const segmentation = await net.segmentPerson(img);
+    const segmentation = await net.segmentPerson(img.elt);
     console.log(segmentation);
-    const coloredPartImage = bodyPix.toColoredPartMask(segmentation);
-    const opacity = 0.7;
-    const flipHorizontal = false;
+    const foregroundColor = { r: 0, g: 0, b: 0, a: 0 };
+    const backgroundColor = { r: 0, g: 0, b: 0, a: 0 };
+    const coloredPartImage = bodyPix.toMask(
+      segmentation,
+      foregroundColor,
+      backgroundColor,
+      true
+    );
+    const opacity = 1;
     const maskBlurAmount = 0;
-    const pixelCellWidth = 10.0;
-    const canvas = document.getElementById(`canvas-${id}`);
-    bodyPix.drawPixelatedMask(
-      canvas,
-      img,
+    const flipHorizontal = false;
+    bodyPix.drawMask(
+      canvas.elt,
+      img.elt,
       coloredPartImage,
       opacity,
       maskBlurAmount,
-      flipHorizontal,
-      pixelCellWidth
+      flipHorizontal
     );
   });
 }
