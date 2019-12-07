315695a56f10a3905298cedebda3c8afbb80743c
Andrew Liu <andrewliustudio@gmail.com>
2019-12-07 00:59:43 -0500

    wip

--- week13/index.js
+++ week13/index.js
@@ -2,6 +2,10 @@
 // http://localhost:8080/week13/
 
 /**
+ * References:
+ *   ES6 async/await: https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Async_await
+ *   Using async/await with a loop: https://stackoverflow.com/questions/37576685/using-async-await-with-a-foreach-loop
+ *   IIFE: https://en.wikipedia.org/wiki/Immediately_invoked_function_expression
  * TODO:
  * cache by image name
  * url.match(/([^\/]+)(?=\.\w+$)/)[0]
@@ -12,30 +16,134 @@ const bodyPartsBaseUrl = "assets";
 // const bodyPartsBaseUrl =
 //   "https://openprocessing-usercontent.s3.amazonaws.com/files/user188449/visual803548/h7742c0df291332632e44a2cdbca0541d";
 
-const bodyParts = [
-  { url: `${bodyPartsBaseUrl}/arm.jpg` },
-  { url: `${bodyPartsBaseUrl}/leg.jpg` }
-  // { url: `${bodyPartsBaseUrl}/body.jpg` }
-];
+const bodyPartsVisuals = [
+  "arm.jpg",
+  "leg.jpg",
+  "arm.jpg",
+  "leg.jpg",
+  "arm.jpg",
+  "leg.jpg",
+  "arm.jpg",
+  "leg.jpg",
+  "arm.jpg",
+  "arm.jpg",
+  "leg.jpg",
+  "arm.jpg",
+  "leg.jpg",
+  "leg.jpg",
+  "arm.jpg"
+  // "leg.jpg"
+].map(img => ({
+  url: `${bodyPartsBaseUrl}/${img}`,
+  isProcessing: false,
+  bodyPart: undefined,
+  img: undefined,
+  canvas: undefined
+}));
+
+const bodyPartsSounds = {
+  nose: "nose",
+  leftEye: "leftEye",
+  rightEye: "rightEye",
+  leftEar: "leftEar",
+  rightEar: "rightEar",
+  leftShoulder: "leftShoulder",
+  rightShoulder: "rightShoulder",
+  leftElbow: "leftElbow",
+  rightElbow: "rightElbow",
+  leftWrist: "leftWrist",
+  rightWrist: "rightWrist",
+  leftHip: "leftHip",
+  rightHip: "rightHip",
+  leftKnee: "leftKnee",
+  rightKnee: "rightKnee",
+  leftAnkle: "leftAnkle",
+  rightAnkle: "rightAnkle"
+};
 let areExternalScriptsLoaded = false;
 let input;
 
 function setup() {
+  // createCanvas(windowWidth, windowHeight);
   noCanvas();
   initializeBodyParts();
   // TODO: move the file input into a banner on the bottom
   input = createFileInput(handleFile);
   input.position(0, 0);
-  frameRate(1);
+  // frameRate(1);
 }
 
-function draw() {
+async function draw() {
   if (!areExternalScriptsLoaded) {
     loadExternalScripts();
     return;
   }
-  loadAndPredict();
+  // P5 draw() doesn't support async. So we create an IIFE here to embed
+  // our own async code and to ensure the rest of functions can be invoked.
+  (async () => {
+    await loadAndPredict();
+    // makeSound();
+    const wrapThreshold = 5;
+    const amplification = 1.33;
+    const size = windowHeight / wrapThreshold;
+    const extendedWidth = windowWidth + size;
+    bodyPartsVisuals.forEach(({ canvas }, i) => {
+      const move =
+        (frameCount + size * Math.floor(i / wrapThreshold)) % windowWidth;
+      const posX = windowWidth - move - size;
+
+      const posY = (i * size) % windowHeight;
+      canvas
+        .position(posX, posY)
+        .style("width", `${size * amplification}px`)
+        .style("height", `${size * amplification}px`);
+    });
+  })();
   // noLoop();
+}
+
+function makeSound() {}
+
+function getBodyPart(segmentation) {
+  const { part: bodyPart } = segmentation.allPoses[0].keypoints.sort(
+    (a, b) => b.score - a.score
+  )[0];
+  return bodyPart;
+}
+
+async function loadAndPredict() {
+  await Promise.all(
+    bodyPartsVisuals.map(async ({ isProcessing, url, img, canvas }, i) => {
+      // Only calls this expensive classification subroutine once.
+      if (isProcessing) {
+        return;
+      }
+      bodyPartsVisuals[i].isProcessing = true;
+
+      const net = await bodyPix.load();
+      const segmentation = await net.segmentPerson(img.elt);
+      const foregroundColor = { r: 0, g: 0, b: 0, a: 0 };
+      const backgroundColor = { r: 0, g: 0, b: 0, a: 0 };
+      const coloredPartImage = bodyPix.toMask(
+        segmentation,
+        foregroundColor,
+        backgroundColor,
+        true
+      );
+      const opacity = 1;
+      const maskBlurAmount = 0;
+      const flipHorizontal = false;
+      bodyPix.drawMask(
+        canvas.elt,
+        img.elt,
+        coloredPartImage,
+        opacity,
+        maskBlurAmount,
+        flipHorizontal
+      );
+      bodyPartsVisuals[i].bodyPart = getBodyPart(segmentation);
+    })
+  );
 }
 
 function handleFile(file) {
@@ -44,18 +152,18 @@ function handleFile(file) {
   }
   const bodyPart = {};
   createBodyPart(bodyPart, file.data);
-  bodyParts.push(bodyPart);
+  bodyPartsVisuals.push(bodyPart);
 }
 
 function initializeBodyParts() {
-  bodyParts.forEach(bodyPart => {
+  bodyPartsVisuals.forEach(bodyPart => {
     createBodyPart(bodyPart);
   });
 }
 
 function createBodyPart(bodyPart, fileData = undefined) {
   const uuid = `${Math.random()}`.slice(2);
-  // Creates unique id string like 5519497453702618, without this from
+  // Creates a unique id string like 5519497453702618, without this from
   // the second browser load and onwards the console will output:
   // Uncaught (in promise) DOMException: Failed to execute 'texImage2D' on 'WebGL2RenderingContext': Tainted canvases may not be loaded.
   bodyPart.img = createImg(fileData ? fileData : `${bodyPart.url}?${uuid}`)
@@ -73,34 +181,8 @@ function createBodyPart(bodyPart, fileData = undefined) {
     .position(0, 0)
     .show()
     .style("width", "")
-    .style("height", "");
-}
-
-async function loadAndPredict() {
-  bodyParts.forEach(async ({ id, img, canvas }) => {
-    const net = await bodyPix.load();
-    const segmentation = await net.segmentPerson(img.elt);
-    console.log(segmentation);
-    const foregroundColor = { r: 0, g: 0, b: 0, a: 0 };
-    const backgroundColor = { r: 0, g: 0, b: 0, a: 0 };
-    const coloredPartImage = bodyPix.toMask(
-      segmentation,
-      foregroundColor,
-      backgroundColor,
-      true
-    );
-    const opacity = 1;
-    const maskBlurAmount = 0;
-    const flipHorizontal = false;
-    bodyPix.drawMask(
-      canvas.elt,
-      img.elt,
-      coloredPartImage,
-      opacity,
-      maskBlurAmount,
-      flipHorizontal
-    );
-  });
+    .style("height", "")
+    .style("opacity", "0.75");
 }
 
 /**
